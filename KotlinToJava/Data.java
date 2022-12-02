4c9c0a209ac9cb857ec997cbc76ad37f35da5e31
im-una <95046930+im-una@users.noreply.github.com>
2022-12-02 18:35:17 +0900

    Create Data.java

---
+++ KotlinToJava/Data.java
@@ -0,0 +1,228 @@
+package com.example.companymaps.data;
+
+import kotlin.Metadata;
+import kotlin.jvm.internal.Intrinsics;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Metadata(
+   mv = {1, 7, 1},
+   k = 1,
+   d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u0006\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b!\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001BU\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u0012\u0006\u0010\u0006\u001a\u00020\u0005\u0012\u0006\u0010\u0007\u001a\u00020\b\u0012\u0006\u0010\t\u001a\u00020\b\u0012\u0006\u0010\n\u001a\u00020\u0005\u0012\u0006\u0010\u000b\u001a\u00020\u0001\u0012\u0006\u0010\f\u001a\u00020\u0005\u0012\u0006\u0010\r\u001a\u00020\b\u0012\u0006\u0010\u000e\u001a\u00020\b¢\u0006\u0002\u0010\u000fJ\t\u0010\u001e\u001a\u00020\u0003HÆ\u0003J\t\u0010\u001f\u001a\u00020\bHÆ\u0003J\t\u0010 \u001a\u00020\u0005HÆ\u0003J\t\u0010!\u001a\u00020\u0005HÆ\u0003J\t\u0010\"\u001a\u00020\bHÆ\u0003J\t\u0010#\u001a\u00020\bHÆ\u0003J\t\u0010$\u001a\u00020\u0005HÆ\u0003J\t\u0010%\u001a\u00020\u0001HÆ\u0003J\t\u0010&\u001a\u00020\u0005HÆ\u0003J\t\u0010'\u001a\u00020\bHÆ\u0003Jm\u0010(\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00052\b\b\u0002\u0010\u0007\u001a\u00020\b2\b\b\u0002\u0010\t\u001a\u00020\b2\b\b\u0002\u0010\n\u001a\u00020\u00052\b\b\u0002\u0010\u000b\u001a\u00020\u00012\b\b\u0002\u0010\f\u001a\u00020\u00052\b\b\u0002\u0010\r\u001a\u00020\b2\b\b\u0002\u0010\u000e\u001a\u00020\bHÆ\u0001J\u0013\u0010)\u001a\u00020*2\b\u0010+\u001a\u0004\u0018\u00010\u0001HÖ\u0003J\t\u0010,\u001a\u00020\u0003HÖ\u0001J\t\u0010-\u001a\u00020\bHÖ\u0001R\u0011\u0010\u0002\u001a\u00020\u0003¢\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u0011R\u0011\u0010\u0004\u001a\u00020\u0005¢\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0013R\u0011\u0010\u0006\u001a\u00020\u0005¢\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0013R\u0011\u0010\u0007\u001a\u00020\b¢\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0016R\u0011\u0010\t\u001a\u00020\b¢\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\u0016R\u0011\u0010\n\u001a\u00020\u0005¢\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0013R\u0011\u0010\u000b\u001a\u00020\u0001¢\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u001aR\u0011\u0010\f\u001a\u00020\u0005¢\u0006\b\n\u0000\u001a\u0004\b\u001b\u0010\u0013R\u0011\u0010\r\u001a\u00020\b¢\u0006\b\n\u0000\u001a\u0004\b\u001c\u0010\u0016R\u0011\u0010\u000e\u001a\u00020\b¢\u0006\b\n\u0000\u001a\u0004\b\u001d\u0010\u0016¨\u0006."},
+   d2 = {"Lcom/example/companymaps/data/Data;", "", "index", "", "경도", "", "도로명우편번호", "도로명주소", "", "사업장명", "소재지우편번호", "업태구분명", "위도", "전화번호", "지번주소", "(IDDLjava/lang/String;Ljava/lang/String;DLjava/lang/Object;DLjava/lang/String;Ljava/lang/String;)V", "getIndex", "()I", "get경도", "()D", "get도로명우편번호", "get도로명주소", "()Ljava/lang/String;", "get사업장명", "get소재지우편번호", "get업태구분명", "()Ljava/lang/Object;", "get위도", "get전화번호", "get지번주소", "component1", "component10", "component2", "component3", "component4", "component5", "component6", "component7", "component8", "component9", "copy", "equals", "", "other", "hashCode", "toString", "app_debug"}
+)
+public final class Data {
+   private final int index;
+   private final double 경도;
+   private final double 도로명우편번호;
+   @NotNull
+   private final String 도로명주소;
+   @NotNull
+   private final String 사업장명;
+   private final double 소재지우편번호;
+   @NotNull
+   private final Object 업태구분명;
+   private final double 위도;
+   @NotNull
+   private final String 전화번호;
+   @NotNull
+   private final String 지번주소;
+
+   public final int getIndex() {
+      return this.index;
+   }
+
+   public final double get경도() {
+      return this.경도;
+   }
+
+   public final double get도로명우편번호() {
+      return this.도로명우편번호;
+   }
+
+   @NotNull
+   public final String get도로명주소() {
+      return this.도로명주소;
+   }
+
+   @NotNull
+   public final String get사업장명() {
+      return this.사업장명;
+   }
+
+   public final double get소재지우편번호() {
+      return this.소재지우편번호;
+   }
+
+   @NotNull
+   public final Object get업태구분명() {
+      return this.업태구분명;
+   }
+
+   public final double get위도() {
+      return this.위도;
+   }
+
+   @NotNull
+   public final String get전화번호() {
+      return this.전화번호;
+   }
+
+   @NotNull
+   public final String get지번주소() {
+      return this.지번주소;
+   }
+
+   public Data(int index, double 경도, double 도로명우편번호, @NotNull String 도로명주소, @NotNull String 사업장명, double 소재지우편번호, @NotNull Object 업태구분명, double 위도, @NotNull String 전화번호, @NotNull String 지번주소) {
+      Intrinsics.checkNotNullParameter(도로명주소, "도로명주소");
+      Intrinsics.checkNotNullParameter(사업장명, "사업장명");
+      Intrinsics.checkNotNullParameter(업태구분명, "업태구분명");
+      Intrinsics.checkNotNullParameter(전화번호, "전화번호");
+      Intrinsics.checkNotNullParameter(지번주소, "지번주소");
+      super();
+      this.index = index;
+      this.경도 = 경도;
+      this.도로명우편번호 = 도로명우편번호;
+      this.도로명주소 = 도로명주소;
+      this.사업장명 = 사업장명;
+      this.소재지우편번호 = 소재지우편번호;
+      this.업태구분명 = 업태구분명;
+      this.위도 = 위도;
+      this.전화번호 = 전화번호;
+      this.지번주소 = 지번주소;
+   }
+
+   public final int component1() {
+      return this.index;
+   }
+
+   public final double component2() {
+      return this.경도;
+   }
+
+   public final double component3() {
+      return this.도로명우편번호;
+   }
+
+   @NotNull
+   public final String component4() {
+      return this.도로명주소;
+   }
+
+   @NotNull
+   public final String component5() {
+      return this.사업장명;
+   }
+
+   public final double component6() {
+      return this.소재지우편번호;
+   }
+
+   @NotNull
+   public final Object component7() {
+      return this.업태구분명;
+   }
+
+   public final double component8() {
+      return this.위도;
+   }
+
+   @NotNull
+   public final String component9() {
+      return this.전화번호;
+   }
+
+   @NotNull
+   public final String component10() {
+      return this.지번주소;
+   }
+
+   @NotNull
+   public final Data copy(int index, double 경도, double 도로명우편번호, @NotNull String 도로명주소, @NotNull String 사업장명, double 소재지우편번호, @NotNull Object 업태구분명, double 위도, @NotNull String 전화번호, @NotNull String 지번주소) {
+      Intrinsics.checkNotNullParameter(도로명주소, "도로명주소");
+      Intrinsics.checkNotNullParameter(사업장명, "사업장명");
+      Intrinsics.checkNotNullParameter(업태구분명, "업태구분명");
+      Intrinsics.checkNotNullParameter(전화번호, "전화번호");
+      Intrinsics.checkNotNullParameter(지번주소, "지번주소");
+      return new Data(index, 경도, 도로명우편번호, 도로명주소, 사업장명, 소재지우편번호, 업태구분명, 위도, 전화번호, 지번주소);
+   }
+
+   // $FF: synthetic method
+   public static Data copy$default(Data var0, int var1, double var2, double var4, String var6, String var7, double var8, Object var10, double var11, String var13, String var14, int var15, Object var16) {
+      if ((var15 & 1) != 0) {
+         var1 = var0.index;
+      }
+
+      if ((var15 & 2) != 0) {
+         var2 = var0.경도;
+      }
+
+      if ((var15 & 4) != 0) {
+         var4 = var0.도로명우편번호;
+      }
+
+      if ((var15 & 8) != 0) {
+         var6 = var0.도로명주소;
+      }
+
+      if ((var15 & 16) != 0) {
+         var7 = var0.사업장명;
+      }
+
+      if ((var15 & 32) != 0) {
+         var8 = var0.소재지우편번호;
+      }
+
+      if ((var15 & 64) != 0) {
+         var10 = var0.업태구분명;
+      }
+
+      if ((var15 & 128) != 0) {
+         var11 = var0.위도;
+      }
+
+      if ((var15 & 256) != 0) {
+         var13 = var0.전화번호;
+      }
+
+      if ((var15 & 512) != 0) {
+         var14 = var0.지번주소;
+      }
+
+      return var0.copy(var1, var2, var4, var6, var7, var8, var10, var11, var13, var14);
+   }
+
+   @NotNull
+   public String toString() {
+      return "Data(index=" + this.index + ", 경도=" + this.경도 + ", 도로명우편번호=" + this.도로명우편번호 + ", 도로명주소=" + this.도로명주소 + ", 사업장명=" + this.사업장명 + ", 소재지우편번호=" + this.소재지우편번호 + ", 업태구분명=" + this.업태구분명 + ", 위도=" + this.위도 + ", 전화번호=" + this.전화번호 + ", 지번주소=" + this.지번주소 + ")";
+   }
+
+   public int hashCode() {
+      int var10000 = ((Integer.hashCode(this.index) * 31 + Double.hashCode(this.경도)) * 31 + Double.hashCode(this.도로명우편번호)) * 31;
+      String var10001 = this.도로명주소;
+      var10000 = (var10000 + (var10001 != null ? var10001.hashCode() : 0)) * 31;
+      var10001 = this.사업장명;
+      var10000 = ((var10000 + (var10001 != null ? var10001.hashCode() : 0)) * 31 + Double.hashCode(this.소재지우편번호)) * 31;
+      Object var1 = this.업태구분명;
+      var10000 = ((var10000 + (var1 != null ? var1.hashCode() : 0)) * 31 + Double.hashCode(this.위도)) * 31;
+      var10001 = this.전화번호;
+      var10000 = (var10000 + (var10001 != null ? var10001.hashCode() : 0)) * 31;
+      var10001 = this.지번주소;
+      return var10000 + (var10001 != null ? var10001.hashCode() : 0);
+   }
+
+   public boolean equals(@Nullable Object var1) {
+      if (this != var1) {
+         if (var1 instanceof Data) {
+            Data var2 = (Data)var1;
+            if (this.index == var2.index && Double.compare(this.경도, var2.경도) == 0 && Double.compare(this.도로명우편번호, var2.도로명우편번호) == 0 && Intrinsics.areEqual(this.도로명주소, var2.도로명주소) && Intrinsics.areEqual(this.사업장명, var2.사업장명) && Double.compare(this.소재지우편번호, var2.소재지우편번호) == 0 && Intrinsics.areEqual(this.업태구분명, var2.업태구분명) && Double.compare(this.위도, var2.위도) == 0 && Intrinsics.areEqual(this.전화번호, var2.전화번호) && Intrinsics.areEqual(this.지번주소, var2.지번주소)) {
+               return true;
+            }
+         }
+
+         return false;
+      } else {
+         return true;
+      }
+   }
+}
